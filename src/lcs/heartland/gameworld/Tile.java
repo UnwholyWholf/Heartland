159f388b4dc1f9a3fac87b691551518a23d1f5ed
Scott Blessing <scott.blessing@hotmail.com>
2014-01-14 20:52:04 -0600

    More tile things

--- src/lcs/heartland/gameworld/Tile.java
+++ src/lcs/heartland/gameworld/Tile.java
@@ -4,38 +4,38 @@ public class Tile
 {
 	//	PUBLIC TILE VARIABLES
 	public enum Foreground
-	{	EMPTY,TREE,ROCK,FLOWER_RED,FLOWER_YELLOW;	}
+	{	EMPTY,TREE,ROCK,FLOWER_RED,FLOWER_YELLOW,BEAR;	}
 	
 	public enum Background
 	{	GRASS,SAND,WATER;	}
 	
-	Foreground foreground;
-	Background background;
+	// private variables
+	private Foreground foreground;
+	private Background background;
 	
-	public Tile()
-	{
-		foreground = Foreground.EMPTY;
-		background = Background.GRASS;
-	}
-	
+	// Constructors
 	public Tile(Foreground f, Background b)
 	{
 		foreground = f;
 		background = b;
 	}
-	
 	public Tile(Foreground f)
 	{
 		foreground = f;
 		background = Background.GRASS;
 	}
-	
 	public Tile(Background b)
 	{
 		foreground = Foreground.EMPTY;
 		background = b;
 	}
-	
+	public Tile()
+	{
+		foreground = Foreground.EMPTY;
+		background = Background.GRASS;
+	}
+
+	// Set-Gets
 	public Foreground getForeground()
 	{
 		return foreground;
